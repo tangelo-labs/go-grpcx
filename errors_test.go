d66fdb25cae530d5874d4f9ee67ac61422720562
Chris Castro <y2k2000@gmail.com>
2024-01-16 12:33:58 +0100

    lint fixes

--- errors_test.go
+++ errors_test.go
@@ -12,26 +12,26 @@ import (
 )
 
 var (
-	dummySentinelOne   = errors.New("dummy one")
-	dummySentinelTwo   = errors.New("dummy two")
-	dummySentinelThree = errors.New("dummy three")
-	dummySentinelFour  = errors.New("dummy four")
+	errDummySentinelOne   = errors.New("dummy one")
+	errDummySentinelTwo   = errors.New("dummy two")
+	errDummySentinelThree = errors.New("dummy three")
+	errDummySentinelFour  = errors.New("dummy four")
 )
 
 func TestNewErrorMapper(t *testing.T) {
 	em := grpcx.NewErrorMapper(codes.Unknown).
 		With(codes.Canceled, context.Canceled).
 		With(codes.DeadlineExceeded, context.DeadlineExceeded).
-		With(codes.InvalidArgument, dummySentinelOne).
-		With(codes.PermissionDenied, dummySentinelTwo).
-		With(codes.NotFound, dummySentinelThree, dummySentinelFour)
+		With(codes.InvalidArgument, errDummySentinelOne).
+		With(codes.PermissionDenied, errDummySentinelTwo).
+		With(codes.NotFound, errDummySentinelThree, errDummySentinelFour)
 
 	require.EqualValues(t, codes.Canceled, status.Code(em.Map(context.Canceled)))
 	require.EqualValues(t, codes.DeadlineExceeded, status.Code(em.Map(context.DeadlineExceeded)))
-	require.EqualValues(t, codes.InvalidArgument, status.Code(em.Map(dummySentinelOne)))
-	require.EqualValues(t, codes.PermissionDenied, status.Code(em.Map(dummySentinelTwo)))
-	require.EqualValues(t, codes.NotFound, status.Code(em.Map(dummySentinelThree)))
-	require.EqualValues(t, codes.NotFound, status.Code(em.Map(dummySentinelFour)))
+	require.EqualValues(t, codes.InvalidArgument, status.Code(em.Map(errDummySentinelOne)))
+	require.EqualValues(t, codes.PermissionDenied, status.Code(em.Map(errDummySentinelTwo)))
+	require.EqualValues(t, codes.NotFound, status.Code(em.Map(errDummySentinelThree)))
+	require.EqualValues(t, codes.NotFound, status.Code(em.Map(errDummySentinelFour)))
 }
 
 func TestNewBaseErrorMapper(t *testing.T) {
@@ -39,5 +39,5 @@ func TestNewBaseErrorMapper(t *testing.T) {
 
 	require.EqualValues(t, codes.Canceled, status.Code(em.Map(context.Canceled)))
 	require.EqualValues(t, codes.DeadlineExceeded, status.Code(em.Map(context.DeadlineExceeded)))
-	require.EqualValues(t, codes.Unknown, status.Code(em.Map(dummySentinelOne)))
+	require.EqualValues(t, codes.Unknown, status.Code(em.Map(errDummySentinelOne)))
 }
