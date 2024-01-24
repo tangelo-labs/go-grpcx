2476ea700578d2a2b1f79f4adbe21d1648855e11
Chris Castro <y2k2000@gmail.com>
2024-01-24 14:03:33 +0100

    lint fixes

--- interception/chain_test.go
+++ interception/chain_test.go
@@ -12,6 +12,8 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+type ctxKey struct{ v string }
+
 func TestChainUnaryServer(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -22,34 +24,34 @@ func TestChainUnaryServer(t *testing.T) {
 		testValue       = 1
 	)
 
-	ctx = context.WithValue(ctx, "parent", testValue)
+	ctx = context.WithValue(ctx, ctxKey{v: "parent"}, testValue)
 	input := "input"
 	output := "output"
 
 	first := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		requireContextValue(ctx, t, "parent", testValue)
+		requireContextValue(ctx, t, ctxKey{v: "parent"}, testValue)
 		require.Equal(t, parentUnaryInfo, info)
 
-		ctx = context.WithValue(ctx, "first", 1)
+		ctx = context.WithValue(ctx, ctxKey{v: "first"}, 1)
 
 		return handler(ctx, req)
 	}
 
 	second := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		requireContextValue(ctx, t, "parent", testValue)
-		requireContextValue(ctx, t, "first", testValue)
+		requireContextValue(ctx, t, ctxKey{v: "parent"}, testValue)
+		requireContextValue(ctx, t, ctxKey{v: "first"}, testValue)
 		require.Equal(t, parentUnaryInfo, info)
 
-		ctx = context.WithValue(ctx, "second", 1)
+		ctx = context.WithValue(ctx, ctxKey{v: "second"}, 1)
 
 		return handler(ctx, req)
 	}
 
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		require.EqualValues(t, input, req)
-		requireContextValue(ctx, t, "parent", testValue)
-		requireContextValue(ctx, t, "first", testValue)
-		requireContextValue(ctx, t, "second", testValue)
+		requireContextValue(ctx, t, ctxKey{v: "parent"}, testValue)
+		requireContextValue(ctx, t, ctxKey{v: "first"}, testValue)
+		requireContextValue(ctx, t, ctxKey{v: "second"}, testValue)
 
 		return output, nil
 	}
@@ -71,7 +73,7 @@ func TestChainStreamServerServer(t *testing.T) {
 		testValue        = 1
 	)
 
-	ctx = context.WithValue(ctx, "parent", testValue)
+	ctx = context.WithValue(ctx, ctxKey{v: "parent"}, testValue)
 
 	t.Run("it should do nothing when no interceptors provided", func(t *testing.T) {
 		require.NoError(t, interception.ChainServerStream()(nil, &fakeServerStream{}, nil, func(svc interface{}, stream grpc.ServerStream) error { return nil }))
@@ -79,23 +81,23 @@ func TestChainStreamServerServer(t *testing.T) {
 
 	t.Run("it should chain interceptors", func(t *testing.T) {
 		first := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-			ctx = context.WithValue(ctx, "first", 1)
+			ctx = context.WithValue(ctx, ctxKey{v: "first"}, 1)
 			stream = grpcx.ServerStreamWithContext(ctx, stream)
 
 			return handler(srv, stream)
 		}
 
 		second := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-			ctx = context.WithValue(ctx, "second", 1)
+			ctx = context.WithValue(ctx, ctxKey{v: "second"}, 1)
 			stream = grpcx.ServerStreamWithContext(ctx, stream)
 
 			return handler(srv, stream)
 		}
 
 		handler := func(svc interface{}, stream grpc.ServerStream) error {
-			requireContextValue(ctx, t, "parent", testValue)
-			requireContextValue(ctx, t, "first", testValue)
-			requireContextValue(ctx, t, "second", testValue)
+			requireContextValue(ctx, t, ctxKey{v: "parent"}, testValue)
+			requireContextValue(ctx, t, ctxKey{v: "first"}, testValue)
+			requireContextValue(ctx, t, ctxKey{v: "second"}, testValue)
 
 			return nil
 		}
@@ -108,7 +110,7 @@ func TestChainStreamServerServer(t *testing.T) {
 	})
 }
 
-func requireContextValue(ctx context.Context, t *testing.T, key string, testValue int, msg ...interface{}) {
+func requireContextValue(ctx context.Context, t *testing.T, key ctxKey, testValue int, msg ...interface{}) {
 	val := ctx.Value(key)
 
 	require.NotNil(t, val, msg...)
