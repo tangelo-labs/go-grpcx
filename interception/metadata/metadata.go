7a4461a57299864a50705783e301985398fd62e9
leannico <leandro.barrientos@gmail.com>
2025-04-11 10:26:48 +0200

    fix staticcheck lint

--- interception/metadata/metadata.go
+++ interception/metadata/metadata.go
@@ -8,20 +8,22 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
+type contextKey string
+
 // UnaryClientInterceptor returns a new unary client interceptor that injects the
 // correlation id from the context into the outgoing metadata. If the correlation
 // id is not present in the context, it will be ignored and the request will
 // proceed without it.
-func UnaryClientInterceptor(k string) grpc.UnaryClientInterceptor {
+func UnaryClientInterceptor(key string) grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		md, err := contextMetadata(ctx)
 		if err != nil {
 			return invoker(ctx, method, req, reply, cc, opts...)
 		}
 
-		val := md.Get(k)
+		val := md.Get(key)
 		if len(val) > 0 {
-			ctx = context.WithValue(ctx, k, val[0])
+			ctx = context.WithValue(ctx, contextKey(key), val[0])
 		}
 
 		return invoker(ctx, method, req, reply, cc, opts...)
@@ -30,19 +32,19 @@ func UnaryClientInterceptor(k string) grpc.UnaryClientInterceptor {
 
 // StreamClientInterceptor similar to UnaryClientInterceptor, but for streaming
 // requests.
-func StreamClientInterceptor(k string) grpc.StreamClientInterceptor {
+func StreamClientInterceptor(key string) grpc.StreamClientInterceptor {
 	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
 		md, err := contextMetadata(ctx)
 		if err != nil {
 			return streamer(ctx, desc, cc, method, opts...)
 		}
 
-		val := md.Get(k)
+		val := md.Get(key)
 		if len(val) != 1 {
 			return streamer(ctx, desc, cc, method, opts...)
 		}
 
-		ctx = context.WithValue(ctx, k, val[0])
+		ctx = context.WithValue(ctx, contextKey(key), val[0])
 
 		return streamer(ctx, desc, cc, method, opts...)
 	}
